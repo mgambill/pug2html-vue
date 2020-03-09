7df41cad90d90384daa189ad6002aa5dd11b05b1
Matt Gambill <46201338+mgambill@users.noreply.github.com>
2020-03-08 22:47:45 -0400

    initial commit

---
+++ src/components/SplitPanel.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+.wrapper.flex.h-screen.bg-gray-50
+  section(ref="left")
+    .h-full.flex-grow-1
+      slot(name="left")
+
+  section(ref="right")
+    .h-full.flex-grow-1
+      slot(name="right")
+</template>
+
+<script>
+export default {
+  name: "SplitPanel",
+  mounted() {
+    //eslint-disable-next-line
+    Split([this.$refs.left, this.$refs.right], { minSize: 0 });
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.gutter {
+  cursor: e-resize;
+  background: #a6a9b7;
+}
+.gutter:hover {
+  background: #5a5f73;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,59 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+
+  SplitPanel
+    template(v-slot:left)
+      MonacoEditor.w-full.h-full(v-model='code', language='html', :options='options')
+    template(v-slot:right)
+      MonacoEditor.w-full.h-full(v-model='pug', language='html', :options='options')
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MonacoEditor from "vue-monaco";
+import SplitPanel from "../components/SplitPanel";
+import { Parser } from "../Parser";
 
+const code = `<div class="grid-cols-2">
+  <MonacoEditor
+    class="editor"
+    v-model="code"
+    language="html"
+    :options="options"
+  />
+  <MonacoEditor
+    class="editor"
+    v-model="code"
+    language="html"
+    :options="options"
+  />
+  </div>`;
 export default {
-  name: "Home",
   components: {
-    HelloWorld
+    MonacoEditor,
+    SplitPanel
+  },
+
+  data() {
+    return {
+      code,
+      parser: new Parser(),
+      options: {
+        language: "html",
+        lineNumbers: "on",
+        roundedSelection: false,
+        scrollBeyondLastLine: false,
+        readOnly: false,
+        theme: "vs-dark",
+        automaticLayout: true
+      }
+    };
+  },
+  watch: {
+    code() {}
+  },
+  computed: {
+    pug() {
+      return this.parser.parse(this.code);
+    }
   }
 };
 </script>
